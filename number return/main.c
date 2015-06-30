63aac18e80cfee8c5769a44a7dc73b368c6e67ee
Auston <auston.salvana@live.com>
2015-06-30 16:00:48 -0700

    Initial Commit

---
+++ number return/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  number return
+//
+//  Created by Auston Salvana on 6/30/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
