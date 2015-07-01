0ddc1130783e089775e2e5b569d1cd01002bf2e4
Auston <auston.salvana@live.com>
2015-06-30 23:13:13 -0700

    saved

--- number return/main.c
+++ number return/main.c
@@ -7,9 +7,42 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
+int midPoint(int min, int max){
+    
+    int theMid = (min + max)/2;
+    
+    return theMid;
+}
+
+
+int binarySearch(int key, int array[], int min, int max){
+    
+    int newMid = midPoint(min, max);
+    
+    
+    if (array[newMid] < key)  {
+        return binarySearch(key, array, newMid + 1, max);
+    }
+    else if (array[newMid] > key){
+        return binarySearch(key, array, min, newMid - 1);
+    }
+    
+    
+    return newMid;
+    
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int array[1000] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    
+    int index = binarySearch(9, array, 0, 19);
+    
+    printf("%d", index);
+    
     return 0;
 }
